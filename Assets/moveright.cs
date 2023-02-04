0ac8e57a98ef22d39b578062f319cce0d98e3560
neckles422 <124460710+neckles422@users.noreply.github.com>
2023-02-04 12:15:52 -0800

    added unity

---
+++ Assets/moveright.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveright : MonoBehaviour
+{   Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+   rb = GetComponent<Rigidbody2D>();
+
+
+rb.AddForce(transform.right*15);
+}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
