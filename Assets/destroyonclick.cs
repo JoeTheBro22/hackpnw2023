177296d7af44f5ea93bbc6dc7291ed2d84d89ff5
JoeTheBro22 <66972056+JoeTheBro22@users.noreply.github.com>
2023-02-05 15:25:05 -0800

    final submission

--- Assets/destroyonclick.cs
+++ Assets/destroyonclick.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class destroyInSeconds : MonoBehaviour
+public class destroyonclick : MonoBehaviour
 {
-    public int seconds = 2;
+    public bool hasClicked = false;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, seconds);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)){
+            gameObject.AddComponent<fadeOut>();
+        }
     }
 }
