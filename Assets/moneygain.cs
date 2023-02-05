5e8e6eb3d1a6ab781e428d50d3fb56f13a33aedb
JoeTheBro22 <66972056+JoeTheBro22@users.noreply.github.com>
2023-02-05 11:35:32 -0800

    added text that gives +2 when money hit

--- Assets/moneygain.cs
+++ Assets/moneygain.cs
@@ -7,13 +7,24 @@ public class moneygain : MonoBehaviour
     public GameObject scoreText;
     private int score;
     private GameObject GameManager;
+    private GameObject Canvas;
+
+    public GameObject popUpTextPrefab;
     // Start is called before the first frame update
     void Start(){
       GameManager = GameObject.Find("GameManager");
+      Canvas = GameObject.Find("Canvas");
     }
 
     void OnCollisionEnter2D(Collision2D myCol){
       if(myCol.gameObject.tag == "money" && GameManager.GetComponent<GameManager>().moneysEnabled == true){
+        // getting half the width and height of canvas
+        float halfHeight = Canvas.GetComponent<RectTransform>().rect.height/2;
+        float halfWidth = Canvas.GetComponent<RectTransform>().rect.width/2;
+        //new Vector3(transform.position.x*halfWidth*2.0f, transform.position.y*halfHeight*2.0f, 0)
+        
+        Instantiate(popUpTextPrefab, Camera.main.WorldToScreenPoint(transform.position), Quaternion.identity, Canvas.transform);
+
         Destroy(myCol.gameObject);
         score = GameManager.GetComponent<GameManager>().score+=2;
         scoreText.GetComponent<UnityEngine.UI.Text>().text = "score: " + score.ToString();
